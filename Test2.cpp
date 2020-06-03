bed90e34d62d900995bffc1f5bebada1728d4b37
mlodszyrower <44478781+mlodszyrower@users.noreply.github.com>
2020-06-03 10:22:44 +0200

    Main

---
+++ Test2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int first, second;
+    string op;
+    cout << "First number" << endl;
+    cin >> first;
+    cout << "Operator" << endl;
+    cin >> op;
+    cout << "Second number" << endl;
+    cin >> second;
+
+    if (op == "+") {
+        cout << "Result: " << first + second;
+    }
+    else if (op == "-") {
+        cout << "Result: " << first - second;
+    }
+    else if (op == "/") {
+        cout << "Result: " << first / second;
+    }
+    else if (op == "%") {
+        cout << "Result: " << first % second;
+    }
+    else 
+        cout << "Incorrect use" << endl;
+    return 0;
+}
+
